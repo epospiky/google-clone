3a0fabced85b926ada48627ee967f761798b3ae1
Ernest <epospiky@gmail.com>
2024-02-09 23:57:42 +0000

    modified result component to return results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -16,12 +16,13 @@ const Results = () => {
 
     switch (location.pathname) {
         case '/search':
-            return (
+            return ( 
                 <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-                    {/*results?.results?.map(({link, title})=>(
-
-                    ))*/}
-
+                    {results?.results?.map(({link, title}, index)=>(
+                        <div key={index} className='md:w-2/5 w-full'>
+                            <a hr></a>
+                        </div>
+                    ))}
                 </div>
             )
         case '/images':
